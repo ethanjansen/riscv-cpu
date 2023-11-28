1754539f79399b794dd8172106341daded68d073
ethanjansen <ethanjansen69@gmail.com>
2023-11-28 14:00:32 -0800

    added memory

---
+++ ram.vhd
@@ -0,0 +1,45 @@
+-------------------------------------------------------------------------------
+-- Company:        Walla Walla University
+-- Engineer:       Ethan Jansen
+--
+-- Create Date:    13:00:00 11/21/2023
+-- Design Name:    Write-First RAM
+-- Module Name:    ram_wf - ram
+-- Project Name:   MicroprocessorDesign
+-- Target Devices: Artix 7
+-- Description:    
+--!                1024x32 Write-First RAM for use in DPU.
+-- Revision:       Revision 0.01 - File Created
+-------------------------------------------------------------------------------
+-- RAM
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity ram_wf is
+  port
+  (
+    clk   : in std_logic; --! Clock
+    we    : in std_logic; --! Write Enable
+    addr  : in std_logic_vector(9 downto 0); --! Address
+    d_in  : in std_logic_vector(31 downto 0); --! Data in
+    d_out : out std_logic_vector(31 downto 0) --! Data out
+  );
+end ram_wf;
+
+architecture ram_arch of ram_wf is
+    type ram_type is array(1023 downto 0) of std_logic_vector(31 downto 0);
+    signal ram : ram_type;
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if we = '1' then
+                ram(to_integer(unsigned(addr))) <= d_in;
+                d_out <= d_in;
+            else
+                d_out <= ram(to_integer(unsigned(addr)));
+            end if;
+        end if;
+    end process;
+end ram_arch;
---
+++ rom.vhd
@@ -0,0 +1,52 @@
+-------------------------------------------------------------------------------
+-- Company:        Walla Walla University
+-- Engineer:       Ethan Jansen
+--
+-- Create Date:    13:00:00 11/21/2023
+-- Design Name:    Initialized ROM
+-- Module Name:    rom_with_init - rom
+-- Project Name:   MicroprocessorDesign
+-- Target Devices: Artix 7
+-- Description:    
+--!                2048x18 ROM initialized by "rom.data" for Instruction Memory
+-- Revision:       Revision 0.01 - File Created
+-------------------------------------------------------------------------------
+-- ROM
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_textio.all;
+use std.textio.all;
+
+entity rom_with_init is
+    port(
+        clk : in std_logic; --! Clock
+        addr : in std_logic_vector(10 downto 0); --! Address
+        d   : out std_logic_vector(17 downto 0) --! Data Out
+    );
+end rom_with_init;
+
+architecture rom of rom_with_init is
+    type rom_type is array(2047 downto 0) of std_logic_vector(17 downto 0);
+
+    impure function initRomFromFile(romFileName : in string) return rom_type is
+        file romFile : text is in romFileName;
+        variable romFileLine : line;
+        variable rom : rom_type;
+    begin
+        for i in rom_type'range loop
+            readline(romFile, romFileLine);
+            read(romFileLine, rom(i));
+        end loop;
+        return rom;
+    end function;
+
+    signal rom : rom_type := initRomFromFile("rom.data");
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            d <= rom(to_integer(unsigned(addr)));
+        end if;
+    end process;
+end rom;
