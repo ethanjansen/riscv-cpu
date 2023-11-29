51a447bedce53ddded47c0f917dcfebf99a4531c
Ethan Jansen <ethan.jansen@wallawalla.edu>
2023-11-28 16:03:50 -0800

    new

---
+++ flag_handler.vhd
@@ -0,0 +1,32 @@
+-------------------------------------------------------------------------------
+-- Company:        Walla Walla University
+-- Engineer:       Ethan Jansen
+--
+-- Create Date:    15:30:00 11/28/2023
+-- Design Name:    Flag Handler
+-- Module Name:    flag_handler - flags
+-- Project Name:   MicroprocessorDesign
+-- Target Devices: Artix 7
+-- Description:    
+--!                Handles flags from value in Accumulator.
+--!                "00"=eq 0, "01"=lt 0, "10"=gt 0, "11"=neq 0.
+-- Revision:       Revision 0.01 - File Created
+-------------------------------------------------------------------------------
+-- Flag Handler
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity flag_handler is
+  port
+  (
+    a_in     : in std_logic_vector(31 downto 0);
+    flag_out : out std_logic_vector(1 downto 0)
+  );
+end flag_handler;
+
+architecture flags of flag_handler is
+
+begin
+    flag_out(1) <= '1' when (a_in(31)='0' and a_in(30 downto 0)/="0000000000000000000000000000000") else '0';
+    flag_out(0) <= '1' when a_in(31)='1' else '0';
+end flags;
---
+++ led_handler.vhd
@@ -0,0 +1,45 @@
+-------------------------------------------------------------------------------
+-- Company:        Walla Walla University
+-- Engineer:       Ethan Jansen
+--
+-- Create Date:    15:50:00 11/28/2023
+-- Design Name:    LED Handler
+-- Module Name:    led_handler - leds
+-- Project Name:   MicroprocessorDesign
+-- Target Devices: Artix 7
+-- Description:    
+--!                LED Logic with memory for holding output.
+--                 1-bit select for ('1') high bytes or ('0') low bytes.
+-- Revision:       Revision 0.01 - File Created
+-------------------------------------------------------------------------------
+-- LED Handler
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity led_handler is
+  port
+  (
+    clk     : in std_logic; --! Clock
+    we      : in std_logic; --! Write Enable
+    sel     : in std_logic; --! 1-bit Select for ('1') high bytes or ('0') low bytes.
+    data_in : in std_logic_vector(31 downto 0); --! Data Input
+    led_out : out std_logic_vector(15 downto 0) --! LED Output
+  );
+end led_handler;
+
+architecture leds of led_handler is
+
+begin
+  process (clk)
+  begin
+    if rising_edge(clk) then
+      if we = '1' then
+        if sel = '1' then
+          led_out <= data_in(31 downto 16);
+        else
+          led_out <= data_in(15 downto 0);
+        end if;
+      end if;
+    end if;
+  end process;
+end leds;
