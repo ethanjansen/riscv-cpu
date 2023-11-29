7fc1dbeaff403046759f8835e2921942d1b02fb6
ethanjansen <ethanjansen69@gmail.com>
2023-11-29 02:56:38 -0800

    added dpu

--- top_sch_proj.vhd
+++ top_sch_proj.vhd
@@ -1,3 +1,11 @@
+--! {reg: [
+--!   {"bits": 10, "name": "Data", "attr": ["address", "immediate", "offset"]},
+--!   {"bits": 1, "name": "Immediate"},
+--!   {"bits": 2, "name": "general"},
+--!   {"bits": 3, "name": "Function", "attr": ["load", "add", "shift left", "shift right", "and", "or", "xor", "branch", "wait", "seven-segment", "led"]},
+--!   {"bits": 2, "name": "Operation", "attr": ["ALU", "store", "PC", "Display"] }
+--! ]}
+
 -------------------------------------------------------------------------------
 -- Company:        Walla Walla University
 -- Engineer:       Ethan Jansen
@@ -18,15 +26,37 @@ use IEEE.STD_LOGIC_1164.all;
 entity top_sch_proj is
   port
   (
-    clk   : in std_logic;
-    btn   : in std_logic_vector(2 downto 0);
-    sw    : in std_logic_vector(7 downto 0);
-    led   : out std_logic_vector(15 downto 0);
-    cath  : out std_logic_vector(7 downto 0); -- seven-seg display (muxing done outside of processor)
-    anode : out std_logic_vector(4 downto 0)
+    mclk  : in std_logic; --! Master Clock
+    btn   : in std_logic_vector(2 downto 0); --! Push Buttons (0=>reset, 1=>contiue, 2=>sstep)
+    sw    : in std_logic_vector(7 downto 0); --! Toggle Switches
+    sw8   : in std_logic; --! Toggle Switch 8 (toggles high/low byte display on led and sseg)
+    led   : out std_logic_vector(15 downto 0); --! LEDs
+    cath  : out std_logic_vector(7 downto 0); --! Seven-Segment Display Cathode
+    anode : out std_logic_vector(4 downto 0) --! Seven-Segment Display Anode
   );
 end top_sch_proj;
 
 architecture main of top_sch_proj is
+  component data_path_unit is
+    port
+    (
+      clk              : in std_logic; --! Clock
+      ctrl             : in std_logic_vector(7 downto 0); --! Control Signal from DPU
+      data_or_addr_in  : in std_logic_vector(9 downto 0); --! Data or Address in from Controller
+      sw_in            : in std_logic_vector(7 downto 0); --! Data in from Switch
+      high_low_sw      : in std_logic; --! High/Low Display Select from sw8
+      flags            : out std_logic_vector(1 downto 0); --! Flags based on A (1=>"gt 0", 0=>"lt 0")
+      led_out          : out std_logic_vector(15 downto 0); --! LED Output
+      sseg_anode_out   : out std_logic_vector(4 downto 0); --! Seven-Segment Display Anode Output (time multiplexed)
+      sseg_cathode_out : out std_logic_vector(7 downto 0) --! Seven-Segment Display Cathode Output (time multiplexed)
+    );
+  end component;
 begin
+  -- clk will become derived clock rather than mclk in the future.
+  -- Constant inputs for testing. Will be mapped to controller.
+  -- flags will be mapped to controller
+  dpu : data_path_unit port map
+  (
+    clk => mclk, ctrl => X"00", data_or_addr_in => "0000000000", sw_in => sw, high_low_sw => sw8,
+    flags => open, led_out => led, sseg_anode_out => anode, sseg_cathode_out => cath);
 end main;
