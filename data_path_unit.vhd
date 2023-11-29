bbd603eb2cc8b1b74b1f2dcb757cb5e5eb6d8f36
ethanjansen <ethanjansen69@gmail.com>
2023-11-29 00:37:20 -0800

    init

--- data_path_unit.vhd
+++ data_path_unit.vhd
@@ -20,6 +20,7 @@ entity data_path_unit is
   (
     clk              : in std_logic; --! Clock
     data_or_addr_in  : in std_logic_vector(9 downto 0); --! Data or Address in from Controller
+    sw_in            : in std_logic_vector(7 downto 0); --! Data in from Switch
     flags            : out std_logic_vector(1 downto 0); --! Flags based on A (1=>"gt 0", 0=>"lt 0")
     led_out          : out std_logic_vector(15 downto 0); --! LED Output
     sseg_anode_out   : out std_logic_vector(4 downto 0); --! Seven-Segment Display Anode Output (time multiplexed)
@@ -28,6 +29,75 @@ entity data_path_unit is
 end data_path_unit;
 
 architecture dpu of data_path_unit is
+  component accumulator is
+    port
+    (
+      clk : in std_logic; --! Clock
+      d   : in std_logic_vector(31 downto 0); --! Data In
+      q   : out std_logic_vector(31 downto 0) --! Data Out
+    );
+  end component;
+
+  component arithmetic_logic_unit is
+    port
+    (
+      ctrl               : in std_logic_vector(6 downto 0); --! comes from high 7 bits of instruction encoding
+      data1_in, data2_in : in std_logic_vector(31 downto 0); --! assuming immediate sign extensions happen outside of alu
+      data_out           : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
+  component flag_handler is
+    port
+    (
+      a_in     : in std_logic_vector(31 downto 0); --! Accumulator data in
+      flag_out : out std_logic_vector(1 downto 0) --! Flag out
+    );
+  end component;
+
+  component led_handler is
+    port
+    (
+      clk     : in std_logic; --! Clock
+      we      : in std_logic; --! Write Enable
+      sel     : in std_logic; --! 1-bit Select for ('1') high bytes or ('0') low bytes.
+      data_in : in std_logic_vector(31 downto 0); --! Data Input
+      led_out : out std_logic_vector(15 downto 0) --! LED Output
+    );
+  end component;
+
+  component ram_wf is
+    port
+    (
+      clk   : in std_logic; --! Clock
+      we    : in std_logic; --! Write Enable
+      addr  : in std_logic_vector(9 downto 0); --! Address
+      d_in  : in std_logic_vector(31 downto 0); --! Data in
+      d_out : out std_logic_vector(31 downto 0) --! Data out
+    );
+  end component;
+
+  component sign_extender is
+    port
+    (
+      ctrl                : in std_logic_vector(4 downto 0); --! Control Signals: "00000" or "00111" for 8-bit, otherwise 10-bit.
+      data_from_cntrlr_in : in std_logic_vector(9 downto 0); --! 8- to 10-bit Data from Controller
+      data_from_sw_in     : in std_logic_vector(7 downto 0); --! 8-bit Data from Switches
+      data_out            : out std_logic_vector(31 downto 0) --! Data Out
+    );
+  end component;
+
+  component sseg_handler is
+    port
+    (
+      clk         : in std_logic; --! Clock
+      we          : in std_logic; --! Write Enable
+      sel         : in std_logic; --! 1-bit Select for ('1') high bytes or ('0') low bytes.
+      data_in     : in std_logic_vector(31 downto 0); --! Data Input
+      anode_out   : out std_logic_vector(0 to 4); --! Anode Output
+      cathode_out : out std_logic_vector(7 downto 0) --! Cathode Output
+    );
+  end component;
 
 begin
 
